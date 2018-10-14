f42049cb14c03b4fe57468011336ee573329428d
Eyal Shacham <shacham@swbell.net>
2018-10-14 18:14:28 -0500

    renamed default tabs to: workout, library and settings with different ionic icons

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { LibraryPage } from '../library/library';
+import { SettingsPage } from '../settings/settings';
+import { WorkoutsPage } from '../workouts/workouts';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabWorkoutsRoot = WorkoutsPage;
+  tabLibraryRoot = LibraryPage;
+  tabSettingsRoot = SettingsPage;
 
   constructor() {
 

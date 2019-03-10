4474e2e5f26a7f4626d65c86115e752b3198cf6f
Eyal Shacham <eyal.shacham@emerson.com>
2019-03-10 10:11:25 -0500

    updated library tab page to capture imgaes from camera/photo library - failing copying file to app local folder

--- src/pages/library/library-page.module.ts
+++ src/pages/library/library-page.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LibraryPage } from './library';
-
- import { Camera } from '@ionic-native/camera';
- import { File } from '@ionic-native/file';
+import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { FilePath } from '@ionic-native/file-path';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
 	declarations: [
@@ -11,6 +13,7 @@ import { LibraryPage } from './library';
 	],
 	imports: [
 		IonicPageModule.forChild(LibraryPage),
+		IonicStorageModule.forRoot()
 	],
 	exports: [
 		LibraryPage,
@@ -20,7 +23,9 @@ import { LibraryPage } from './library';
 	],
 	providers: [
 		Camera,
-		File
+		File,
+		WebView,
+		FilePath
 	]
 })
 export class LibraryPageModule {}
